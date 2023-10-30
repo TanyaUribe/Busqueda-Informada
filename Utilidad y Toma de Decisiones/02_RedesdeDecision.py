a9beabbd9e117d1abca556047f8b649ce4bfaaec
Tanya Uribe <126315885+TanyaUribe@users.noreply.github.com>
2023-10-30 12:18:20 -0600

    Update 02_RedesdeDecision.py

--- Utilidad y Toma de Decisiones/02_RedesdeDecision.py
+++ Utilidad y Toma de Decisiones/02_RedesdeDecision.py
@@ -1,25 +1,45 @@
+"""
+hemos creado una red de decisión simple que modela la decisión de si llevar una sombrilla
+en función de si llueve, si hay tráfico y si llegas tarde. Luego, calculamos el valor esperado
+de tomar la decisión de llevar una sombrilla. 
+"""
 
-from pgmpy.models import BayesianNetwork
-from pgmpy.factors.discrete import TabularCPD
+import networkx as nx
 
-# Crear la red bayesiana
-bn = BayesianNetwork([('A', 'B'), ('A', 'C')])
+# Crear un gráfico de red de decisión
+G = nx.DiGraph()
 
-# Crear tablas de probabilidad condicional para los nodos A, B y C
-cpd_a = TabularCPD(variable='A', variable_card=2, values=[[0.6], [0.4]])
-cpd_b = TabularCPD(variable='B', variable_card=2, values=[[0.7, 0.3], [0.2, 0.8]], evidence=['A'], evidence_card=[2])
-cpd_c = TabularCPD(variable='C', variable_card=2, values=[[0.5, 0.5], [0.4, 0.6]], evidence=['A'], evidence_card=[2])
+# Agregar nodos (eventos y decisiones)
+G.add_node("Iniciar", type="decision")
+G.add_node("Llueve", type="event")
+G.add_node("Tráfico", type="event")
+G.add_node("Llegar tarde", type="event")
+G.add_node("Tomar sombrilla", type="decision")
 
-bn.add_cpds(cpd_a, cpd_b, cpd_c)
+# Agregar bordes entre los nodos
+G.add_edge("Iniciar", "Llueve")
+G.add_edge("Iniciar", "Tráfico")
+G.add_edge("Llueve", "Tomar sombrilla")
+G.add_edge("Tráfico", "Llegar tarde")
+G.add_edge("Llegar tarde", "Tomar sombrilla")
 
-# Agregar un nodo de decisión
-bn.add_node('D')
+# Asignar probabilidades a los eventos
+G.nodes["Llueve"]["prob"] = 0.3
+G.nodes["Tráfico"]["prob"] = 0.4
+G.nodes["Llegar tarde"]["prob"] = 0.2
 
-# Crear la tabla de probabilidad condicional para el nodo de decisión D
-cpd_d = TabularCPD(variable='D', variable_card=2, values=[[0.4], [0.6]])
+# Asignar valores a las decisiones
+G.nodes["Iniciar"]["value"] = 0
+G.nodes["Tomar sombrilla"]["value"] = 5
 
-bn.add_cpds(cpd_d)
+# Calcular el valor esperado
+def calcular_valor_esperado(G):
+    valor_esperado = 0
+    for nodo in nx.topological_sort(G):
+        if G.nodes[nodo]["type"] == "event":
+            padre = list(G.predecessors(nodo))[0]
+            valor_esperado += G.nodes[nodo]["prob"] * G.nodes[padre]["value"]
+    return valor_esperado
 
-# Imprimir información sobre la red bayesiana
-print("Red Bayesiana:")
-print(bn.get_cpds())
+valor_esperado = calcular_valor_esperado(G)
+print(f"El valor esperado de la red de decisión es: {valor_esperado}")
